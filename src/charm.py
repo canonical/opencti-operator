ef098d2f7cdafa53ed19eb90245e92ea1d9e66b8
Weii Wang <weii.wang@canonical.com>
2025-08-08 23:19:30 +0800

    Force refresh the ingress elation data on every event

--- src/charm.py
+++ src/charm.py
@@ -198,6 +198,12 @@ class OpenCTICharm(ops.CharmBase):
             relation_name="ingress",
             port=8080,
         )
+        # Sometimes the ingress library doesn't properly handle pod
+        # restarts,which can cause the IP field inside the ingress
+        # relation data to become stale, resulting in ingress failures.
+        # As a workaround, force refresh the ingress relation data
+        # (especially the ip field) on every event.
+        ingress._publish_auto_data()
         self.framework.observe(ingress.on.ready, self._reconcile)
         self.framework.observe(ingress.on.revoked, self._reconcile)
         return ingress
