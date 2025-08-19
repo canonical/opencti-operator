018be796bd034f2f087ed33423f79e550a07c734
Arturo Seijas <arturo.seijas@canonical.com>
2025-08-19 12:30:15 +0200

    Switch from microk8s to Canonical kubernetes

--- tests/integration/prepare.sh
+++ tests/integration/prepare.sh
@@ -6,20 +6,16 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
-# OpenSearch/RabbitMQ charms are deployed on lxd and OpenCTI charm is deployed on microk8s.
+# OpenSearch/RabbitMQ charms are deployed on lxd and OpenCTI charm is deployed on Canonical kubernetes.
 
 TESTING_MODEL="$(juju switch)"
 
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
-# Change microk8s default file limits
-echo "ulimit -n 458752" | sudo tee -a /var/snap/microk8s/current/args/containerd-env
-sudo snap restart microk8s
-sg snap_microk8s -c "microk8s status --wait-ready"
-sg snap_microk8s -c "juju bootstrap localhost localhost"
+juju bootstrap localhost localhost
 
 echo "Switching to testing model"
-sg snap_microk8s -c "juju switch $TESTING_MODEL"
+juju switch $TESTING_MODEL
 
 # https://charmhub.io/opensearch/docs/t-set-up#set-parameters-on-the-host-machine
 sudo tee -a /etc/sysctl.conf > /dev/null <<EOT
