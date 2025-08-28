d62baa28c6a80375f22d37a0d3d96496c15e1252
jjimenezgarcia <jose.jimenez@canonical.com>
2025-08-28 10:08:04 +0200

    [-+] Custom configs of NTI connector

--- connectors/nti/src/charm.py
+++ connectors/nti/src/charm.py
@@ -9,17 +9,36 @@ import pathlib
 
 import ops
 
+import logging
+logger = logging.getLogger(__name__)
+
 from charms.opencti.v0.opencti_connector import OpenctiConnectorCharm
 
 
 class OpenctiNtiConnectorCharm(OpenctiConnectorCharm):
     connector_type = "EXTERNAL_IMPORT"
+    VALID_LOG_LEVELS = {"debug", "info", "warn", "error"}
 
     @property
     def charm_dir(self) -> pathlib.Path:
         return pathlib.Path(__file__).parent.parent.absolute()
 
-    
+    def _gen_env(self) -> dict[str, str]:
+        env = super()._gen_env()
+
+        # Force nti
+        env["CONNECTOR_SCOPE"] = "nti"
+
+        # Check log level value, logging and setting to default if there is a misconfiguration
+        log_level = env.get("CONNECTOR_LOG_LEVEL", "info")
+        if log_level not in self.VALID_LOG_LEVELS:
+            logging.warning(
+                f"CONNECTOR_LOG_LEVEL '{log_level}' is invalid, using 'info' instead."
+            )
+            log_level = "info"
+        env["CONNECTOR_LOG_LEVEL"] = log_level
+
+        return env
 
 if __name__ == "__main__":
     ops.main(OpenctiNtiConnectorCharm)
