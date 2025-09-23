ef1edc7c1531e163aac088eaea897b08b88ff863
Arturo Seijas <arturo.seijas@canonical.com>
2025-09-23 09:18:38 +0200

    Strip path prefix when requiring ingress

--- src/charm.py
+++ src/charm.py
@@ -197,6 +197,7 @@ class OpenCTICharm(ops.CharmBase):
             self,
             relation_name="ingress",
             port=8080,
+            strip_prfix=True,
         )
         # Sometimes the ingress library doesn't properly handle pod
         # restarts,which can cause the IP field inside the ingress
