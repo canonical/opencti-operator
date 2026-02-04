77d5d28283c60d74486ccd3778d75f0ae56c221d
Arturo Seijas <arturo.seijas@canonical.com>
2026-02-04 10:02:24 +0100

    fix typo

--- scripts/gen_connector_charm.py
+++ scripts/gen_connector_charm.py
@@ -332,7 +332,7 @@ def extract_crowdstrike_configs(doc_url: str) -> dict:
         description = row["description"]
         if not is_mandatory:
             description = "(optional) " + description
-        config_type = "int" if (row.get("example", "").isdigit() or row.get("default", "")].isdigit()) else "string"
+        config_type = "int" if (row.get("example", "").isdigit() or row.get("default", "").isdigit()) else "string"
         result[constant_to_kebab(name)] = {
             "description": description,
             "type": config_type,
