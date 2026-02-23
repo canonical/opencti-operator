90fa5d15370a368fcffb4790a4c43725c8cdb285
Sofya Smolyakova <sofya.smolyakova@canonical.com>
2026-02-23 10:09:28 -0500

    Added update_pebble function to zero out relation-based configuration

--- connectors/woap/src/charm.py
+++ connectors/woap/src/charm.py
@@ -79,7 +79,7 @@ class OpenctiWoapConnectorCharm(OpenctiConnectorCharm):
                 status_msg = "Waiting for: " + ", ".join(missing_requirements)
                 self.unit.status = ops.WaitingStatus(status_msg)
                 try:
-                    self._reconcile_connector()
+                    self.update_pebble(missing_requirements)
                 except Exception:
                     pass
                 #return here because we cannot configure the workload yet
@@ -93,6 +93,51 @@ class OpenctiWoapConnectorCharm(OpenctiConnectorCharm):
             self.unit.status = ops.WaitingStatus(str(exc))
         except Blocked as exc:
             self.unit.status = ops.BlockedStatus(str(exc))
+
+    def update_pebble(self, missing_requirements) -> None:
+        """Zero out relation-based configuration and stop the service."""
+        container = self.unit.get_container(self.meta.name)
+        if not container.can_connect():
+            return
+        
+        try:
+            # wrapping this because super()._gen_env() will raise NotReady 
+            # if the OpenCTI relation is gone.
+            env = super()._gen_env()
+        except NotReady:
+            env = {}
+
+        for requirement in missing_requirements:
+            if requirement == "OpenCTI relation":
+                # Remove all OpenCTI related environment variables
+                env.pop("OPENCTI_URL", None)
+                env.pop("OPENCTI_TOKEN", None)
+            elif requirement == "OpenSearch relation":
+                # Remove all OpenSearch related environment variables
+                env.pop("OPENSEARCH_HOST", None)
+                env.pop("OPENSEARCH_PORT", None)
+                env.pop("OPENSEARCH_USER", None)
+                env.pop("OPENSEARCH_PASSWORD", None)
+            elif requirement == "OpenSearch credentials":
+                # Remove all OpenSearch credential environment variables
+                env.pop("OPENSEARCH_USER", None)
+                env.pop("OPENSEARCH_PASSWORD", None)
+
+        #Create a layer that disables the service and clears the env
+        cleanup_layer = ops.pebble.LayerDict(
+            services={
+                "connector": {
+                    "override": "replace",
+                    "startup": "disabled",  # Stop the service from running/restarting
+                    "environment": env,
+                }
+            }
+        )
+
+        #Push the layer and replan
+        container.add_layer("connector", cleanup_layer, combine=True)
+        container.replan()
+        logger.info("Cleaning up Pebble layer due to missing requirements")
 
 
     def _gen_env(self) -> dict[str, str]:
